954091d4cdd2716415a03d3585fea914b2ae0059
James Grecian <james.grecian@gmail.com>
2019-03-13 16:27:33 -0230

    Add text to ui.r

--- ui.R
+++ ui.R
@@ -5,6 +5,9 @@
 #
 
 ui <- fluidPage(
+  titlePanel("harpMap"),
+    p("In March 2019 researchers from the University of St Andrews and Canadian Department for Fisheries and Oceans deployed 10 satellite transmitters on harp seals."),
+    p("These tags allow us to track the seals as they migrate north to follow the seasonal sea ice retreat."),
   leafletOutput("mymap", height = 800),
-  p()
+  p("This work was made possible by a UK-Canada Arctic Partnership Bursary from the NERC Arctic Office and funding from DFO.")
 )
