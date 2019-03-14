d382a09edab1ff477034b990ffbdf80bb60eb53b
James Grecian <james.grecian@gmail.com>
2019-03-14 10:29:58 -0230

    Add sponsor logos

--- ui.R
+++ ui.R
@@ -13,7 +13,7 @@ ui <- fluidPage(
     "at the University of St Andrews and the ",
     a("Marine Mammal Section",
       href = "http://www.dfo-mpo.gc.ca/science/coe-cde/cemam/index-eng.html"),
-    "of the Canadian Department for Fisheries and Oceans deployed 10 satellite transmitters on harp seals."),
+    "of the Canadian Department for Fisheries and Oceans deployed 10 satellite transmitters on harp seals in the Gulf of St Lawrence."),
   p("These tags allow the scientists to track the seals as they migrate north to follow the seasonal sea ice retreat."),
   br(),
   leafletOutput("mymap", height = 800),
@@ -24,6 +24,12 @@ ui <- fluidPage(
     "and funding from the ",
     a("DFO",
       href = "http://www.dfo-mpo.gc.ca/index-eng.htm")),
-  br()
+  br(),
+  fluidRow(
+    img(src='Arctic-Office-Logo.png', align = "centre", height = 60),
+    img(src='DFO.png', align = "centre", height = 80),
+    img(src='SMRU.jpg', align = "centre", height = 60),
+    img(src='University-St-Andrews.png', align = "centre", height = 80)
+  )
 )
 
