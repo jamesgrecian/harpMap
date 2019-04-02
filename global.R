d444e385e81ecdb1f13131a09a1944dd91932833
James Grecian <james.grecian@gmail.com>
2019-04-02 14:44:54 +0100

    Switch location source from server to dropbox

--- global.R
+++ global.R
@@ -17,32 +17,40 @@ require(httr)
 require(rgdal)
 #devtools::install_github('andrewsali/shinycssloaders')
 require(shinycssloaders)
+require(rdrop2)
 
 # Load data from SMRU server and format for plotting using leaflet
 # define data url
-harps <- read_html("http://www.smru.st-andrews.ac.uk/Instrumentation/php/location.php?gref=hp6&seq=82955EBB1425FAA9E053F909FB8AEE36") 
+#harps <- read_html("http://www.smru.st-andrews.ac.uk/Instrumentation/php/location.php?gref=hp6&seq=82955EBB1425FAA9E053F909FB8AEE36") 
 
 # find tag ids
-ids <- harps %>% html_nodes('h3') %>% html_text()
+#ids <- harps %>% html_nodes('h3') %>% html_text()
 # extract most recent locations
-harps <- harps %>% html_nodes('table') %>% html_table()
+#harps <- harps %>% html_nodes('table') %>% html_table()
 # add id to each dataframe
-harps <- mapply(cbind, harps, "id" = ids, SIMPLIFY = F)
+#harps <- mapply(cbind, harps, "id" = ids, SIMPLIFY = F)
 # unlist
-harps <- dplyr::bind_rows(harps)
+#harps <- dplyr::bind_rows(harps)
 
 # rearrange columns
-harps <- harps %>% dplyr::select("id", "Received (UTC)", "LQ", "Latitude", "Longitude", "No. mess", "Best level", "Vmask")
-names(harps)[2] <- "datetime_utc"
+#harps <- harps %>% dplyr::select("id", "Received (UTC)", "LQ", "Latitude", "Longitude", "No. mess", "Best level", "Vmask")
+#names(harps)[2] <- "datetime_utc"
 # convert to tibble
-harps <- as_tibble(harps)
-harps <- harps %>% mutate(datetime_utc = ymd_hms(datetime_utc))
+#harps <- as_tibble(harps)
+#harps <- harps %>% mutate(datetime_utc = ymd_hms(datetime_utc))
 #order by time
-harps <- harps %>% arrange(id, datetime_utc)
+#harps <- harps %>% arrange(id, datetime_utc)
 #filter out data prior to deployment
-harps <- harps %>% filter(datetime_utc > "2019-03-22 00:00:01")
+#harps <- harps %>% filter(datetime_utc > "2019-03-22 00:00:01")
 
 # run foisGras?
+# is it possible to load from dropbox...?
+harps <- drop_read_csv("hp6_raw_locs.csv")
+harps <- harps %>% rename("id" = id,
+                      "datetime_utc" = date,
+                      "LQ" = lc,
+                      "Longitude" = lon,
+                      "Latitude" = lat)
 
 # set up palette
 pal <- colorFactor(viridis_pal(option = "D")(length(unique(harps$id))),
